59ca027ab1216a90d727a08b4d6b3b3b13f8dd37
Levy <levisilvas2018@gmail.com>
2022-03-03 16:58:34 -0300

    first API - commit inicial

---
+++ MyMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace firstWebApp
+{
+    public class MyMiddleware
+    {
+
+        private readonly RequestDelegate _next;
+        public MyMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            
+            Console.Write("\n----------------- Aperte qualquer tecla para prosseguir:  ---------------\n");
+            string aperteTecla = Console.ReadLine();
+
+            await _next(context);
+
+            Console.WriteLine("\n---------------------------- Processo Finalizado! --------------------------\n");
+        }
+    }
+
+    public static class MyMiddlewareExtensions 
+    {
+        // Extension method
+        public static IApplicationBuilder UseMyMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<MyMiddleware>();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+## Primeiro contato com web api.
+
+
+
+- Api básica feita em asp.net core, aprendendo sobre estrutura do projeto, app.settings, arquivos .json, dependencies, nuget, etc...
+
+- Projeto feito no [Desenvolvedor.io](https://desenvolvedor.io/curso-online-iniciando-com-asp-net-core) para melhor compreensão do programa de bootcamp da [Squadra Digital](https://inspire.squadra.com.br/programa-new-thinkers).
+
+  
+
+  
+
+
+
